dfd2dafa01459567bb6a6036f2dfbcf70fd5760f
Khung Long Bao Chua <emailcuamayban@gmail.com>
2020-02-11 22:41:57 +0700

    sidebar left

--- resources/js/components/NavBar/NavBarComponent.js
+++ resources/js/components/NavBar/NavBarComponent.js
@@ -1,90 +1,99 @@
-
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
 import MenuList from "@material-ui/core/MenuList";
+import Drawer from "@material-ui/core/Drawer";
+
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
 
 const useStyles = makeStyles(theme => ({
+    list: {
+        width: 250
+    },
     grow: {
-        flexGrow: 1,
+        flexGrow: 1
     },
     menuButton: {
-        marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2)
     },
     title: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
+        display: "none",
+        [theme.breakpoints.up("sm")]: {
+            display: "block"
+        }
     },
     search: {
-        position: 'relative',
+        position: "relative",
         borderRadius: theme.shape.borderRadius,
         backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
+        "&:hover": {
+            backgroundColor: fade(theme.palette.common.white, 0.25)
         },
         marginRight: theme.spacing(2),
         marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
             marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
+            width: "auto"
+        }
     },
     searchIcon: {
         width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
     },
     inputRoot: {
-        color: 'inherit',
+        color: "inherit"
     },
     inputInput: {
         padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
             width: 200,
-            '&:focus': {
-                width: 300,
-            },
-        },
-
-
+            "&:focus": {
+                width: 300
+            }
+        }
     },
     sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
-        },
+        display: "none",
+        [theme.breakpoints.up("md")]: {
+            display: "flex"
+        }
     },
     sectionMobile: {
-        display: 'flex',
-        [theme.breakpoints.up('md')]: {
-            display: 'none',
-        },
+        display: "flex",
+        [theme.breakpoints.up("md")]: {
+            display: "none"
+        }
     },
     menuFlex: {
-        display: 'flex',
-        right: '20px'
+        display: "flex",
+        right: "20px"
     }
 }));
 
@@ -113,14 +122,62 @@ export default function NavBarComponent() {
         setMobileMoreAnchorEl(event.currentTarget);
     };
 
-    const menuId = 'primary-search-account-menu';
+    const [state, setState] = React.useState({
+        left: false
+    });
+
+    const handleToggleSidebar = (side, open) => event => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
+
+        setState({ ...state, [side]: open });
+    };
+
+    const sideList = side => (
+        <div
+            className={classes.list}
+            role="presentation"
+            onClick={handleToggleSidebar(side, false)}
+            onKeyDown={handleToggleSidebar(side, false)}
+        >
+            <List>
+                {["Inbox", "Starred", "Send email", "Drafts"].map(
+                    (text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    )
+                )}
+            </List>
+            <Divider />
+            <List>
+                {["All mail", "Trash", "Spam"].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    const menuId = "primary-search-account-menu";
     const renderMenu = (
         <Menu
             anchorEl={anchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
             id={menuId}
             keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
             open={isMenuOpen}
             onClose={handleMenuClose}
         >
@@ -129,14 +186,14 @@ export default function NavBarComponent() {
         </Menu>
     );
 
-    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const mobileMenuId = "primary-search-account-menu-mobile";
     const renderMobileMenu = (
         <Menu
             anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
             id={mobileMenuId}
             keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
             open={isMobileMenuOpen}
             onClose={handleMobileMenuClose}
         >
@@ -149,7 +206,10 @@ export default function NavBarComponent() {
                 <p>Messages</p>
             </MenuItem>
             <MenuItem>
-                <IconButton aria-label="show 11 new notifications" color="inherit">
+                <IconButton
+                    aria-label="show 11 new notifications"
+                    color="inherit"
+                >
                     <Badge badgeContent={11} color="secondary">
                         <NotificationsIcon />
                     </Badge>
@@ -172,7 +232,12 @@ export default function NavBarComponent() {
 
     return (
         <div className={classes.grow}>
-
+            <Drawer
+                open={state.left}
+                onClose={handleToggleSidebar("left", false)}
+            >
+                {sideList("left")}
+            </Drawer>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
@@ -181,7 +246,7 @@ export default function NavBarComponent() {
                         color="inherit"
                         aria-label="open drawer"
                     >
-                        <MenuIcon />
+                        <MenuIcon onClick={handleToggleSidebar("left", true)} />
                     </IconButton>
                     <Typography className={classes.title} variant="h6" noWrap>
                         Material-UI
@@ -194,25 +259,30 @@ export default function NavBarComponent() {
                             placeholder="Search…"
                             classes={{
                                 root: classes.inputRoot,
-                                input: classes.inputInput,
+                                input: classes.inputInput
                             }}
-                            inputProps={{ 'aria-label': 'search' }}
+                            inputProps={{ "aria-label": "search" }}
                         />
                     </div>
                     <div className={classes.grow} />
                     <div className={classes.sectionDesktop}>
                         <MenuList className={classes.menuFlex}>
-
                             <MenuItem>Home</MenuItem>
                             <MenuItem>About</MenuItem>
                             <MenuItem>Services</MenuItem>
                         </MenuList>
-                        <IconButton aria-label="show 4 new mails" color="inherit">
+                        <IconButton
+                            aria-label="show 4 new mails"
+                            color="inherit"
+                        >
                             <Badge badgeContent={4} color="secondary">
                                 <MailIcon />
                             </Badge>
                         </IconButton>
-                        <IconButton aria-label="show 17 new notifications" color="inherit">
+                        <IconButton
+                            aria-label="show 17 new notifications"
+                            color="inherit"
+                        >
                             <Badge badgeContent={17} color="secondary">
                                 <NotificationsIcon />
                             </Badge>
